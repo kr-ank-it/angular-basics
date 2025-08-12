47c8609e0be09ed395a6b05d85257c0cff546820
Ankit <ankit.kr9819@gmail.com>
2025-08-13 02:48:03 +0530

    Basics: Signals, properties

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,43 @@
-import { Component, signal } from '@angular/core';
+import {Component, effect, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Login} from './login/login';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Login],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
   protected readonly title = signal('Products');
+  userDetails = "Ankit";
+  // counter: number = 0;
+  counter= signal(10);
+  inputValue: string = '';
+
+  constructor() {
+    effect(() => {
+      console.log('Signal received:', this.counter());
+    });
+  }
+
+  increment() {
+    this.counter.set(this.counter() + 1);
+  }
+  decrement() {
+    this.counter.set(this.counter() - 1);
+  }
+  reset() {
+    this.counter.set(0);
+  }
+
+  handleInputPress(event: Event) {
+    const inputElementValue = (event.target as HTMLInputElement).value;
+    console.log(inputElementValue)
+    this.inputValue = inputElementValue;
+  }
+
+  handleEmailPress(value: string) {
+    console.log(value);
+  }
 }
