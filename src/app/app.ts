3172ca5ada82decfdc121f171d7306271e10325f
Ankit <ankit.kr9819@gmail.com>
2025-08-13 09:13:41 +0530

    Directives

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,12 @@
 import {Component, effect, signal} from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import {Login} from './login/login';
+import {FormsModule} from '@angular/forms';
+import {NgFor} from '@angular/common';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, Login],
+  imports: [RouterOutlet, Login, FormsModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
@@ -14,6 +16,8 @@ export class App {
   // counter: number = 0;
   counter= signal(10);
   inputValue: string = '';
+  taskName: string = '';
+  taskList: {id:number, name:string}[]=[];
 
   constructor() {
     effect(() => {
@@ -40,4 +44,23 @@ export class App {
   handleEmailPress(value: string) {
     console.log(value);
   }
+
+
+  createTask() {
+    if(this.taskName.trim() === '') {
+      return;
+    }
+    const newTask = {
+      id: this.taskList.length + 1,
+      name: this.taskName
+    };
+    this.taskList.push(newTask);
+    this.taskName = ''; // Clear the input field after adding the task
+  }
+
+  removeTask(taskId: number) {
+    this.taskList = this.taskList.filter(task => task.id !== taskId);
+
+  }
+
 }
