126624fb7124e577fd844c6e7bd3e5a784bc1e47
Ankit <ankit.kr9819@gmail.com>
2025-08-14 12:38:40 +0530

    Reactive form: Sign-up

--- src/app/signup/signup.ts
+++ src/app/signup/signup.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-signup',
-  imports: [],
+  imports: [
+    ReactiveFormsModule
+  ],
   templateUrl: './signup.html',
   styleUrl: './signup.css'
 })
 export class Signup {
+  signupForm = new FormGroup({
+    username: new FormControl('', [Validators.required, Validators.minLength(3)]),
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [Validators.required, Validators.minLength(6)]),
+  });
 
+  get username() {
+    return this.signupForm.get("username");
+  }
+  get email() {
+    return this.signupForm.get("email");
+  }
+  onSubmit() {
+    console.log(this.signupForm.value);
+  }
 }
